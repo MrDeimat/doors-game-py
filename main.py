d38a35488210af64f1106762cb8b574bb5171ed7
MrDeimat <mrdeimat9@gmail.com>
2024-11-24 15:38:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import time
 from art import *
 
 import random
+mm = 0
 hp = 3
 bot = 0
 player = 0
-mm = 0
-door = 0
 
 
 print('''
@@ -52,18 +51,39 @@ if mm == 1:
     time.sleep(0.3)
     print(Fore.GREEN + 'GO')
     print('round 1')
+    print('1/2')
     player = int(input('choose door'))
-    random.random(1, 2)
-    bot = random
-    if bot >> player:
-        hp = hp - 1
-        print('round 2')
-        player = int(input('choose door'))
-        random.random(1, 2)
-        bot = random
+    bot = random.randint(1,2)
     if player == bot:
+        print('You Lost 1 HP')
+        hp = hp =- 1
+    else:
+        print('round 2')
+        print('1/2/3')
         player = int(input('choose door'))
-        random.random(1, 2)
-        bot = random
+        bot = random.randint(1, 2)
+        if player == bot:
+            print('You Lost 1 HP')
+            hp = hp = - 1
+        else:
+            print('round 3')
+            print('1/2/3')
+            player = int(input('choose door'))
+            bot = random.randint(1, 3)
+            if player == bot:
+                print('You Lost 1 HP')
+                hp = hp = - 1
+            else:
+                print('round 4')
+                print('1/2/3/4')
+                player = int(input('choose door'))
+                bot = random.randint(1, 4)
+                
+                if player == bot:
+                    print('You Lost 1 HP')
+                    hp = hp = - 1
+                    
+
 else:
     print('🗿')
+
